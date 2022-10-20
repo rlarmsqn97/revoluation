d92da46633a87baf1e75d15baa2641a65bbb9ee2
rmsqn <srmsqn@kyungmin.ac.kr>
2022-10-21 00:55:57 +0900

    p.77 테스트코드 작성

--- src/test/java/com/boo/springboot/web/HelloControllerTest.java
+++ src/test/java/com/boo/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.boo.springboot.web;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -28,5 +27,19 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // 응답 본문의 내용 검증 "hello" return인지 아닌지
     }
 
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 
 }
