3daa226d488e0985a910e58f50ad74df40eff88d
rmsqn <srmsqn@kyungmin.ac.kr>
2022-10-29 00:44:54 +0900

    p.100 gradle 설정 변경 및 테스트코드 작성

---
+++ src/main/java/com/boo/springboot/web/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.boo.springboot.web.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity // 테이블과 링크될 클래스
+public class Posts {
+    @Id // Pk 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
